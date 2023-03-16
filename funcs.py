d58e5a82802b9f8be68a2754a84f2ee2943208b4
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-03-16 18:31:50 +0000

    esqueleto do projeto

---
+++ funcs.py
@@ -0,0 +1,19 @@
+class funcs:
+
+    def __init__(self):
+        pass
+
+    def create_database(self):
+        pass
+
+    def read_database(self):
+        pass
+
+    def clean_data(self):
+        pass
+
+    def view_data(self):
+        pass
+
+    def analyze_data(self):
+        pass
